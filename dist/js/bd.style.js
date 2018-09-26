3cd0780878c94b3eb7daddc6f651b10551413649
RC816 <caiyitian816@126.com>
2018-09-26 16:16:48 +0800

    update

---
+++ dist/js/bd.style.js
@@ -0,0 +1,33 @@
+var bdStyle=
+
+    [
+        {
+            "featureType": "all",
+            "elementType": "all",
+            "stylers": {
+                "lightness": 10,
+                "saturation": -100
+            }
+        },
+        {
+            "featureType": "poilabel",
+            "elementType": "all",
+            "stylers": {
+                "visibility": "on"
+            }
+        },
+        {
+            "featureType": "road",
+            "elementType": "labels",
+            "stylers": {
+                "visibility": "off"
+            }
+        },
+        {
+            "featureType": "poilabel",
+            "elementType": "all",
+            "stylers": {
+                "visibility": "off"
+            }
+        }
+    ]
